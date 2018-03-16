d08e9472bb0af597e292b48dbe3af29091ad5522
YjjTT <yjjtt@yjjtt-2.local>
2018-03-16 15:52:23 +0800

    Initial Commit

---
+++ YjjMatchBarChatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YjjMatchBarChatDemo
+//
+//  Created by YjjTT on 2018/3/16.
+//  Copyright © 2018年 YjjTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YjjMatchBarChatDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YjjMatchBarChatDemo
+//
+//  Created by YjjTT on 2018/3/16.
+//  Copyright © 2018年 YjjTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
